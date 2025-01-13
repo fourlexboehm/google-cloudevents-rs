695df213a25f492f946023c23488fca2a8fbc12c
Alex Boehm <alex@sy.local>
2025-01-13 16:19:44 +1030

    Bump version

--- src/google/api/mod.rs
+++ src/google/api/mod.rs
@@ -19,7 +19,6 @@ pub struct LabelDescriptor {
 pub mod label_descriptor {
     /// Value types that can be used as label values.
     #[derive(serde::Serialize, serde::Deserialize)]
-    #[serde(default)]
     #[serde(rename_all = "snake_case")]
     #[derive(
         Clone,
@@ -67,7 +66,6 @@ pub mod label_descriptor {
 /// The launch stage as defined by [Google Cloud Platform
 /// Launch Stages](<https://cloud.google.com/terms/launch-stages>).
 #[derive(serde::Serialize, serde::Deserialize)]
-#[serde(default)]
 #[serde(rename_all = "snake_case")]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
